5e09c7e5385e39a5b00389ad73ad150412468d67
docktermj <michael@dockter.com>
2022-12-19 11:07:06 -0500

    #3 Savepoint

--- g2configserver/g2configserver.go
+++ g2configserver/g2configserver.go
@@ -31,55 +31,72 @@ func getG2config() *g2sdk.G2configImpl {
 // ----------------------------------------------------------------------------
 
 func (server *G2ConfigServer) AddDataSource(ctx context.Context, request *pb.AddDataSourceRequest) (*pb.AddDataSourceResponse, error) {
-	var err error = nil
-	response := pb.AddDataSourceResponse{}
+	g2config := getG2config()
+	result, err := g2config.AddDataSource(ctx, uintptr(request.GetConfigHandle()), request.GetInputJson())
+	response := pb.AddDataSourceResponse{
+		Result: result,
+	}
 	return &response, err
 }
 
 func (server *G2ConfigServer) Close(ctx context.Context, request *pb.CloseRequest) (*pb.CloseResponse, error) {
-	var err error = nil
+	g2config := getG2config()
+	err := g2config.Close(ctx, uintptr(request.GetConfigHandle()))
 	response := pb.CloseResponse{}
 	return &response, err
 }
 
 func (server *G2ConfigServer) Create(ctx context.Context, request *pb.CreateRequest) (*pb.CreateResponse, error) {
-	var err error = nil
-	response := pb.CreateResponse{}
+	g2config := getG2config()
+	result, err := g2config.Create(ctx)
+	response := pb.CreateResponse{
+		Result: int64(result),
+	}
 	return &response, err
 }
 
 func (server *G2ConfigServer) DeleteDataSource(ctx context.Context, request *pb.DeleteDataSourceRequest) (*pb.DeleteDataSourceResponse, error) {
-	var err error = nil
+	g2config := getG2config()
+	err := g2config.DeleteDataSource(ctx, uintptr(request.GetConfigHandle()), request.GetInputJson())
 	response := pb.DeleteDataSourceResponse{}
 	return &response, err
 }
 
 func (server *G2ConfigServer) Destroy(ctx context.Context, request *pb.DestroyRequest) (*pb.DestroyResponse, error) {
-	var err error = nil
+	g2config := getG2config()
+	err := g2config.Destroy(ctx)
 	response := pb.DestroyResponse{}
 	return &response, err
 }
 
 func (server *G2ConfigServer) Init(ctx context.Context, request *pb.InitRequest) (*pb.InitResponse, error) {
-	var err error = nil
+	g2config := getG2config()
+	err := g2config.Init(ctx, request.GetModuleName(), request.GetIniParams(), int(request.GetVerboseLogging()))
 	response := pb.InitResponse{}
 	return &response, err
 }
 
 func (server *G2ConfigServer) ListDataSources(ctx context.Context, request *pb.ListDataSourcesRequest) (*pb.ListDataSourcesResponse, error) {
-	var err error = nil
-	response := pb.ListDataSourcesResponse{}
+	g2config := getG2config()
+	result, err := g2config.ListDataSources(ctx, uintptr(request.GetConfigHandle()))
+	response := pb.ListDataSourcesResponse{
+		Result: result,
+	}
 	return &response, err
 }
 
 func (server *G2ConfigServer) Load(ctx context.Context, request *pb.LoadRequest) (*pb.LoadResponse, error) {
-	var err error = nil
+	g2config := getG2config()
+	err := g2config.Load(ctx, uintptr(request.GetConfigHandle()), (request.GetJsonConfig()))
 	response := pb.LoadResponse{}
 	return &response, err
 }
 
 func (server *G2ConfigServer) Save(ctx context.Context, request *pb.SaveRequest) (*pb.SaveResponse, error) {
-	var err error = nil
-	response := pb.SaveResponse{}
+	g2config := getG2config()
+	result, err := g2config.Save(ctx, uintptr(request.GetConfigHandle()))
+	response := pb.SaveResponse{
+		Result: result,
+	}
 	return &response, err
 }
