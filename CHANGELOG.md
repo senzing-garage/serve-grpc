780750e5e42b877951a1a90ab70b0ef1a341c68a
Michael Dockter <docktermj@users.noreply.github.com>
2023-04-21 16:10:43 -0400

    #83 Prepare for versioned release (#84)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 -
 
+## [0.4.2] - 2023-04-21
+
+### Changed in 0.4.2
+
+- Update dependencies
+  - github.com/senzing/g2-sdk-go v0.6.1
+  - github.com/senzing/g2-sdk-go-base v0.1.8
+
 ## [0.4.1] - 2023-04-18
 
 ### Changed in 0.4.1
--- cmd/github.go
+++ cmd/github.go
@@ -2,11 +2,10 @@
 // Created by make-go-github-file.yaml on Tue Apr 18 14:52:58 UTC 2023
 package cmd
 
-var githubDate            string = "2023-04-18"
-var githubIteration       string = "0"
-var githubRef             string = "refs/tags/0.4.2"
-var githubRefName         string = "0.4.2"
-var githubRepository      string = "Senzing/serve-grpc"
-var githubRepositoryName  string = "serve-grpc"
-var githubVersion         string = "0.4.2"
-
+var githubDate string = "2023-04-21"
+var githubIteration string = "0"
+var githubRef string = "refs/tags/0.4.2"
+var githubRefName string = "0.4.2"
+var githubRepository string = "Senzing/serve-grpc"
+var githubRepositoryName string = "serve-grpc"
+var githubVersion string = "0.4.2"
