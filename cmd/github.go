e2689bf9c08df5440399f914dece000991b29278
docktermj <michael@dockter.com>
2023-05-11 17:04:01 -0400

    #83 Prepare for versioned release

--- cmd/github.go
+++ cmd/github.go
@@ -2,11 +2,10 @@
 // Created by make-go-github-file.yaml on Wed May 10 21:59:28 UTC 2023
 package cmd
 
-var githubDate            string = "2023-05-10"
-var githubIteration       string = "0"
-var githubRef             string = "refs/tags/0.4.4"
-var githubRefName         string = "0.4.4"
-var githubRepository      string = "Senzing/serve-grpc"
-var githubRepositoryName  string = "serve-grpc"
-var githubVersion         string = "0.4.4"
-
+var githubDate string = "2023-05-11"
+var githubIteration string = "0"
+var githubRef string = "refs/tags/0.4.4"
+var githubRefName string = "0.4.4"
+var githubRepository string = "Senzing/serve-grpc"
+var githubRepositoryName string = "serve-grpc"
+var githubVersion string = "0.4.4"
