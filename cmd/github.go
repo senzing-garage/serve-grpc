225a878f46f3cc1286d363ed0d0c905da312711a
docktermj <docktermj@users.noreply.github.com>
2023-05-19 17:26:50 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.4.7

--- cmd/github.go
+++ cmd/github.go
@@ -1,12 +1,12 @@
-// 0.4.6
-// Created by make-go-github-file.yaml on Wed May 17 19:30:21 UTC 2023
+// 0.4.7
+// Created by make-go-github-file.yaml on Fri May 19 17:26:50 UTC 2023
 package cmd
 
-var githubDate            string = "2023-05-17"
+var githubDate            string = "2023-05-19"
 var githubIteration       string = "0"
-var githubRef             string = "refs/tags/0.4.6"
-var githubRefName         string = "0.4.6"
+var githubRef             string = "refs/tags/0.4.7"
+var githubRefName         string = "0.4.7"
 var githubRepository      string = "Senzing/serve-grpc"
 var githubRepositoryName  string = "serve-grpc"
-var githubVersion         string = "0.4.6"
+var githubVersion         string = "0.4.7"
 
