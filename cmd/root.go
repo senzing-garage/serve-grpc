e388a18c1ad7a3f21901352ab1bad5dd4248e23a
docktermj <michael@dockter.com>
2023-06-12 12:11:47 -0400

    #113 Refactor context variables

--- cmd/root.go
+++ cmd/root.go
@@ -34,118 +34,118 @@ For more information, visit https://github.com/Senzing/serve-grpc
 // ----------------------------------------------------------------------------
 
 var ContextBools = []struct {
-	Dfault bool
-	Envar  string
-	Help   string
-	Option string
+	Default bool
+	Envar   string
+	Help    string
+	Option  string
 }{
 	{
-		Dfault: false,
-		Envar:  envar.EnableG2config,
-		Help:   help.EnableG2config,
-		Option: option.EnableG2config,
+		Default: false,
+		Envar:   envar.EnableG2config,
+		Help:    help.EnableG2config,
+		Option:  option.EnableG2config,
 	},
 	{
-		Dfault: false,
-		Envar:  envar.EnableG2configmgr,
-		Help:   help.EnableG2configmgr,
-		Option: option.EnableG2configmgr,
+		Default: false,
+		Envar:   envar.EnableG2configmgr,
+		Help:    help.EnableG2configmgr,
+		Option:  option.EnableG2configmgr,
 	},
 	{
-		Dfault: false,
-		Envar:  envar.EnableG2diagnostic,
-		Help:   help.EnableG2diagnostic,
-		Option: option.EnableG2diagnostic,
+		Default: false,
+		Envar:   envar.EnableG2diagnostic,
+		Help:    help.EnableG2diagnostic,
+		Option:  option.EnableG2diagnostic,
 	},
 	{
-		Dfault: false,
-		Envar:  envar.EnableG2engine,
-		Help:   help.EnableG2engine,
-		Option: option.EnableG2engine,
+		Default: false,
+		Envar:   envar.EnableG2engine,
+		Help:    help.EnableG2engine,
+		Option:  option.EnableG2engine,
 	},
 	{
-		Dfault: false,
-		Envar:  envar.EnableG2product,
-		Help:   help.EnableG2product,
-		Option: option.EnableG2product,
+		Default: false,
+		Envar:   envar.EnableG2product,
+		Help:    help.EnableG2product,
+		Option:  option.EnableG2product,
 	},
 }
 
 var ContextInts = []struct {
-	Dfault int
-	Envar  string
-	Help   string
-	Option string
+	Default int
+	Envar   string
+	Help    string
+	Option  string
 }{
 	{
-		Dfault: 0,
-		Envar:  envar.EngineLogLevel,
-		Help:   help.EngineLogLevel,
-		Option: option.EngineLogLevel,
+		Default: 0,
+		Envar:   envar.EngineLogLevel,
+		Help:    help.EngineLogLevel,
+		Option:  option.EngineLogLevel,
 	},
 	{
-		Dfault: 8258,
-		Envar:  envar.GrpcPort,
-		Help:   help.GrpcPort,
-		Option: option.GrpcPort,
+		Default: 8258,
+		Envar:   envar.GrpcPort,
+		Help:    help.GrpcPort,
+		Option:  option.GrpcPort,
 	},
 }
 
 var ContextStrings = []struct {
-	Dfault string
-	Envar  string
-	Help   string
-	Option string
+	Default string
+	Envar   string
+	Help    string
+	Option  string
 }{
 	{
-		Dfault: "",
-		Envar:  envar.Configuration,
-		Help:   help.Configuration,
-		Option: option.Configuration,
+		Default: "",
+		Envar:   envar.Configuration,
+		Help:    help.Configuration,
+		Option:  option.Configuration,
 	},
 	{
-		Dfault: "",
-		Envar:  envar.DatabaseUrl,
-		Help:   help.DatabaseUrl,
-		Option: option.DatabaseUrl,
+		Default: "",
+		Envar:   envar.DatabaseUrl,
+		Help:    help.DatabaseUrl,
+		Option:  option.DatabaseUrl,
 	},
 	{
-		Dfault: "",
-		Envar:  envar.EngineConfigurationJson,
-		Help:   help.EngineConfigurationJson,
-		Option: option.EngineConfigurationJson,
+		Default: "",
+		Envar:   envar.EngineConfigurationJson,
+		Help:    help.EngineConfigurationJson,
+		Option:  option.EngineConfigurationJson,
 	},
 	{
-		Dfault: fmt.Sprintf("serve-grpc-%d", time.Now().Unix()),
-		Envar:  envar.EngineModuleName,
-		Help:   help.EngineModuleName,
-		Option: option.EngineModuleName,
+		Default: fmt.Sprintf("serve-grpc-%d", time.Now().Unix()),
+		Envar:   envar.EngineModuleName,
+		Help:    help.EngineModuleName,
+		Option:  option.EngineModuleName,
 	},
 	{
-		Dfault: "INFO",
-		Envar:  envar.LogLevel,
-		Help:   help.LogLevel,
-		Option: option.LogLevel,
+		Default: "INFO",
+		Envar:   envar.LogLevel,
+		Help:    help.LogLevel,
+		Option:  option.LogLevel,
 	},
 	{
-		Dfault: "",
-		Envar:  envar.ObserverOrigin,
-		Help:   help.ObserverOrigin,
-		Option: option.ObserverOrigin,
+		Default: "",
+		Envar:   envar.ObserverOrigin,
+		Help:    help.ObserverOrigin,
+		Option:  option.ObserverOrigin,
 	},
 	{
-		Dfault: "",
-		Envar:  envar.ObserverUrl,
-		Help:   help.ObserverUrl,
-		Option: option.ObserverUrl,
+		Default: "",
+		Envar:   envar.ObserverUrl,
+		Help:    help.ObserverUrl,
+		Option:  option.ObserverUrl,
 	},
 }
 
 var ContextStringSlices = []struct {
-	Dfault []string
-	Envar  string
-	Help   string
-	Option string
+	Default []string
+	Envar   string
+	Help    string
+	Option  string
 }{}
 
 // ----------------------------------------------------------------------------
@@ -155,16 +155,16 @@ var ContextStringSlices = []struct {
 // Since init() is always invoked, define command line parameters.
 func init() {
 	for _, contextBool := range ContextBools {
-		RootCmd.Flags().Bool(contextBool.Option, contextBool.Dfault, fmt.Sprintf(contextBool.Help, contextBool.Envar))
+		RootCmd.Flags().Bool(contextBool.Option, contextBool.Default, fmt.Sprintf(contextBool.Help, contextBool.Envar))
 	}
 	for _, contextInt := range ContextInts {
-		RootCmd.Flags().Int(contextInt.Option, contextInt.Dfault, fmt.Sprintf(contextInt.Help, contextInt.Envar))
+		RootCmd.Flags().Int(contextInt.Option, contextInt.Default, fmt.Sprintf(contextInt.Help, contextInt.Envar))
 	}
 	for _, contextString := range ContextStrings {
-		RootCmd.Flags().String(contextString.Option, contextString.Dfault, fmt.Sprintf(contextString.Help, contextString.Envar))
+		RootCmd.Flags().String(contextString.Option, contextString.Default, fmt.Sprintf(contextString.Help, contextString.Envar))
 	}
 	for _, contextStringSlice := range ContextStringSlices {
-		RootCmd.Flags().StringSlice(contextStringSlice.Option, contextStringSlice.Dfault, fmt.Sprintf(contextStringSlice.Help, contextStringSlice.Envar))
+		RootCmd.Flags().StringSlice(contextStringSlice.Option, contextStringSlice.Default, fmt.Sprintf(contextStringSlice.Help, contextStringSlice.Envar))
 	}
 }
 
@@ -214,7 +214,7 @@ func loadOptions(cobraCommand *cobra.Command) {
 	// Bools
 
 	for _, contextVar := range ContextBools {
-		viper.SetDefault(contextVar.Option, contextVar.Dfault)
+		viper.SetDefault(contextVar.Option, contextVar.Default)
 		err = viper.BindPFlag(contextVar.Option, cobraCommand.Flags().Lookup(contextVar.Option))
 		if err != nil {
 			panic(err)
@@ -224,7 +224,7 @@ func loadOptions(cobraCommand *cobra.Command) {
 	// Ints
 
 	for _, contextVar := range ContextInts {
-		viper.SetDefault(contextVar.Option, contextVar.Dfault)
+		viper.SetDefault(contextVar.Option, contextVar.Default)
 		err = viper.BindPFlag(contextVar.Option, cobraCommand.Flags().Lookup(contextVar.Option))
 		if err != nil {
 			panic(err)
@@ -234,7 +234,7 @@ func loadOptions(cobraCommand *cobra.Command) {
 	// Strings
 
 	for _, contextVar := range ContextStrings {
-		viper.SetDefault(contextVar.Option, contextVar.Dfault)
+		viper.SetDefault(contextVar.Option, contextVar.Default)
 		err = viper.BindPFlag(contextVar.Option, cobraCommand.Flags().Lookup(contextVar.Option))
 		if err != nil {
 			panic(err)
@@ -244,7 +244,7 @@ func loadOptions(cobraCommand *cobra.Command) {
 	// StringSlice
 
 	for _, contextVar := range ContextStringSlices {
-		viper.SetDefault(contextVar.Option, contextVar.Dfault)
+		viper.SetDefault(contextVar.Option, contextVar.Default)
 		err = viper.BindPFlag(contextVar.Option, cobraCommand.Flags().Lookup(contextVar.Option))
 		if err != nil {
 			panic(err)
