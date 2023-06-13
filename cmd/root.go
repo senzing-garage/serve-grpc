f7cef913c555d2a253f21188495cae89d104d31b
docktermj <michael@dockter.com>
2023-06-13 15:56:34 -0400

    #115 Savepoint

--- cmd/root.go
+++ cmd/root.go
@@ -160,7 +160,7 @@ func PreRun(cobraCommand *cobra.Command, args []string) {
 // Used in construction of cobra.Command
 func RunE(_ *cobra.Command, _ []string) error {
 	var err error = nil
-	ctx := context.TODO()
+	ctx := context.Background()
 
 	logLevelName := viper.GetString(option.LogLevel)
 
