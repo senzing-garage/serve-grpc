58979ecb03d3af04b573f1f5e18f7b1405539f36
Sam <109683132+kernelsam@users.noreply.github.com>
2023-07-11 10:16:09 -0700

    Senzing/build-resources#27 add gofmt workflow call (#134)
    * Senzing/build-resources#27 add gofmt workflow call
    
    * Senzing/build-resources#27 fix go formatting

--- cmd/github.go
+++ cmd/github.go
@@ -2,11 +2,10 @@
 // Created by make-go-github-file.yaml on Fri Jun 16 20:31:38 UTC 2023
 package cmd
 
-var githubDate            string = "2023-06-16"
-var githubIteration       string = "0"
-var githubRef             string = "refs/tags/0.4.9"
-var githubRefName         string = "0.4.9"
-var githubRepository      string = "Senzing/serve-grpc"
-var githubRepositoryName  string = "serve-grpc"
-var githubVersion         string = "0.4.9"
-
+var githubDate string = "2023-06-16"
+var githubIteration string = "0"
+var githubRef string = "refs/tags/0.4.9"
+var githubRefName string = "0.4.9"
+var githubRepository string = "Senzing/serve-grpc"
+var githubRepositoryName string = "serve-grpc"
+var githubVersion string = "0.4.9"
