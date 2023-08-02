be898b83ce79703ad7e8945d57448a205dae7120
docktermj <michael@dockter.com>
2023-08-02 17:02:06 -0400

    #157 Adjust variables

--- bin/macos_exec_dyld.sh
+++ bin/macos_exec_dyld.sh
@@ -5,4 +5,13 @@ echo ">>>>>>>>>>>>>>>>>>>> ${LD_LIBRARY_PATH}"
 echo ">>>>>>>>>>>>>>>>>>>> ${DYLD_LIBRARY_PATH}"
 env
 
+
+# SENZING_DIR ?= /opt/senzing/g2
+# SENZING_TOOLS_SENZING_DIRECTORY ?= $(SENZING_DIR)
+
+# LD_LIBRARY_PATH := $(SENZING_TOOLS_SENZING_DIRECTORY)/lib:$(SENZING_TOOLS_SENZING_DIRECTORY)/lib/macos
+# DYLD_LIBRARY_PATH := $(LD_LIBRARY_PATH)
+
+export DYLD_LIBRARY_PATH=/opt/senzing/g2/lib:/opt/senzing/g2/lib/macos:
+
 "$@"
