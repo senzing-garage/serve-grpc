fb4cdade57de214865376f705c3227ec515e4028
docktermj <michael@dockter.com>
2023-08-03 08:56:36 -0400

    #157 CHANGE DYLD_LIBRARY_PATH

--- bin/macos_exec_dyld.sh
+++ bin/macos_exec_dyld.sh
@@ -3,7 +3,4 @@
 export DYLD_LIBRARY_PATH=/opt/senzing/g2/lib:/opt/senzing/g2/lib/macos
 export LD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}
 
-echo ">>>>>>>>>>>>>>>>>>>> ${LD_LIBRARY_PATH}"
-echo ">>>>>>>>>>>>>>>>>>>> ${DYLD_LIBRARY_PATH}"
-
 "$@"
