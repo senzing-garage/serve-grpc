d0ed0ba6a6a68b48cf027fafcb7a2d75ca609502
docktermj@users.noreply.github.com <docktermj@users.noreply.github.com@users.noreply.github.com>
2023-08-18 22:01:14 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.4.15

--- cmd/github.go
+++ cmd/github.go
@@ -1,11 +1,15 @@
-// 0.4.9
-// Created by make-go-github-file.yaml on Fri Jun 16 20:31:38 UTC 2023
+// 0.4.15
+// Created by make-go-github-file.yaml on Fri Aug 18 22:01:14 UTC 2023
+//
+//lint:file-ignore U1000 Ignore all unused code, it's generated
 package cmd
 
-var githubDate string = "2023-07-25"
-var githubIteration string = "0"
-var githubRef string = "refs/tags/0.4.10"
-var githubRefName string = "0.4.10"
-var githubRepository string = "Senzing/serve-grpc"
-var githubRepositoryName string = "serve-grpc"
-var githubVersion string = "0.4.10"
+var (
+	githubDate           string = "2023-08-18"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.4.15"
+	githubRefName        string = "0.4.15"
+	githubRepository     string = "Senzing/serve-grpc"
+	githubRepositoryName string = "serve-grpc"
+	githubVersion        string = "0.4.15"
+)
