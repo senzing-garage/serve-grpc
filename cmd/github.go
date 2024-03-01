cf1574d44991034f1616a6fb94ea168087e14138
docktermj <michael@dockter.com>
2024-03-01 13:09:53 -0500

    Fix tabs

--- cmd/github.go
+++ cmd/github.go
@@ -1,18 +1,3 @@
-// 0.6.1
-// Created by make-go-github-file.yaml on Wed Jan  3 17:22:32 UTC 2024
-//
-//lint:file-ignore U1000 Ignore all unused code, it's generated
-package cmd
-
-var (
-	githubDate           string = "2024-01-03"
-	githubIteration      string = "0"
-	githubRef            string = "refs/tags/0.6.1"
-	githubRefName        string = "0.6.1"
-	githubRepository     string = "senzing-garage/serve-grpc"
-	githubRepositoryName string = "serve-grpc"
-	githubVersion        string = "0.6.1"
-)
 // 0.7.1
 // Created by make-go-github-file.yaml on Fri Mar  1 17:59:38 UTC 2024
 //
@@ -20,11 +5,11 @@ var (
 package cmd
 
 var (
-\tgithubDate           string = "2024-03-01"
-\tgithubIteration      string = "0"
-\tgithubRef            string = "refs/tags/0.7.1"
-\tgithubRefName        string = "0.7.1"
-\tgithubRepository     string = "senzing-garage/serve-grpc"
-\tgithubRepositoryName string = "serve-grpc"
-\tgithubVersion        string = "0.7.1"
+	githubDate           string = "2024-03-01"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.7.1"
+	githubRefName        string = "0.7.1"
+	githubRepository     string = "senzing-garage/serve-grpc"
+	githubRepositoryName string = "serve-grpc"
+	githubVersion        string = "0.7.1"
 )
