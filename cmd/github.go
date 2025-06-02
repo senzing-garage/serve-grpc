d115e620428d54cc51027be5c67154c76c5499aa
senzingdevops <senzingdevops@users.noreply.github.com>
2025-06-02 17:55:15 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.9.12

--- cmd/github.go
+++ cmd/github.go
@@ -1,15 +1,15 @@
-// 0.9.11
-// Created by senzing-factory/github-action-make-go-github-file.yaml on Thu May  8 17:46:05 UTC 2025
+// 0.9.12
+// Created by senzing-factory/github-action-make-go-github-file.yaml on Mon Jun  2 17:55:15 UTC 2025
 //
 //lint:file-ignore U1000 Ignore all unused code, it's generated
 package cmd
 
 var (
-	githubDate           = "2025-05-08"
+	githubDate           = "2025-06-02"
 	githubIteration      = "0"
-	githubRef            = "refs/tags/0.9.11"
-	githubRefName        = "0.9.11"
+	githubRef            = "refs/tags/0.9.12"
+	githubRefName        = "0.9.12"
 	githubRepository     = "senzing-garage/serve-grpc"
 	githubRepositoryName = "serve-grpc"
-	githubVersion        = "0.9.11"
+	githubVersion        = "0.9.12"
 )
